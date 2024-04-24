6b85750cd16e6c0d2b42a56355ec285dac9b42d0
Tatianna21 <165177439+Tatianna21@users.noreply.github.com>
2024-04-24 12:35:45 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+/*
+Student Name: Tatianna Hickson 
+Flie Name:script.js 
+Date:4/15/2024
+*/
+//Hamburger function 
+function hamburger() {
+	var menu = document.getElementById ("menu-links"); 
+	if (menu.style.display === "block") {
+		menu.style.display = "none";
+	}
+	else {
+		menu.style.display = "block";
+	}
+}
+	
+	
